01f64bab702736f1127b925612c403174a4c6671
asharokhin <aleks.sharokhin@gmail.com>
2022-09-01 20:24:53 +0300

    Hello World version

---
+++ package_python.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const spawn = require("child_process").spawn,
+  ls = spawn(
+    "pyinstaller",
+    [
+      "-w",
+      "--onefile",
+      "--distpath dist-python",
+      "backend/run_app.py",
+    ],
+    {
+      shell: true,
+    }
+  );
+
+ls.stdout.on("data", function (data) {
+  // stream output of build process
+  console.log(data.toString());
+});
+
+ls.stderr.on("data", function (data) {
+  console.log("Packaging error: " + data.toString());
+});
+ls.on("exit", function (code) {
+  console.log("child process exited with code " + code.toString());
+});
